5095565ea686f74cc972dd6ce9ab071a03075745
Andre Dublin <81dublin@gmail.com>
2014-04-19 18:00:54 -0400

    update
    Signed-off-by: Andre Dublin <81dublin@gmail.com>

--- website/config/database.php
+++ website/config/database.php
@@ -1,9 +1,33 @@
 <?php defined(BASEPATH) || exit('No direct script access allowed');
 
+// updating and deleting users/posts will be a homework exercise for the students
+
 function getDBConnection()
 {
 
 }
 
-// create, read users
-// crud blog
+function createUser()
+{
+
+}
+
+function getUser()
+{
+
+}
+
+function createPost()
+{
+
+}
+
+function getPost()
+{
+
+}
+
+function getRecentPosts()
+{
+
+}
