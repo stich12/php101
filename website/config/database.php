bdd1574e3a0dd5e44db7764e39d2142a1d6688f8
Andre Dublin <81dublin@gmail.com>
2014-04-19 23:53:44 -0400

    update database script
    Signed-off-by: Andre Dublin <81dublin@gmail.com>

--- website/config/database.php
+++ website/config/database.php
@@ -4,30 +4,100 @@
 
 function getDBConnection()
 {
-
+    try {
+        return new PDO('mysql:dbname=php101;host=127.0.0.1', 'root', 'root');
+    } catch(PDOException $e) {
+        return $e->getMessage();
+    }
 }
 
-function createUser()
+function createUser($user)
 {
+    $db = getDBConnection();
 
+    if ($db instanceof PDO) {
+        $sth = $db->prepare('INSERT INTO \'Users\' VALUES (:username, :password, :color)');
+        $sth->execute(array(
+            ':username' => $db->quote($user['username']),
+            ':password' => $db->quote($user['password']),
+            ':color' => $db->quote($user['color'])
+        ));
+
+        $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($result !== false) {
+            return $user;
+        } else {
+            // return that there was an error
+        }
+    } else {
+        // return that there was an error
+    }
 }
 
-function getUser()
+function getUser($username)
 {
+    $db = getDBConnection();
+    $sth = $db->prepare('SELECT * FROM \'Users\' WHERE username = :username');
+    $sth->execute(array(
+        ':username' => $db->quote($username)
+    ));
 
+    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+    if ($result !== false) {
+        return $result;
+    } else {
+        // return that there was an error
+    }
 }
 
-function createPost()
+function createPost($post)
 {
+    $db = getDBConnection();
+    $sth = $db->prepare('INSERT INTO \'Posts\' VALUES (:title, :datetime, :content)');
+    $sth->execute(array(
+        ':title' => $db->quote($post['title']),
+        ':datetime' => $db->quote($post['datetime']),
+        ':content' => $db->quote($post['content'])
+    ));
 
+    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+    if ($result !== false) {
+        return $result;
+    } else {
+        // return that there was an error
+    }
 }
 
-function getPost()
+function getPost($id)
 {
+    $db = getDBConnection();
+    $sth = $db->prepare('SELECT * FROM \'Posts\' WHERE ID = \':id\'');
+    $sth->execute(array(
+       ':id' => $db->quote($id)
+    ));
 
+    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+    if ($result !== false) {
+        return $result;
+    } else {
+        // return that there was an error
+    }
 }
 
 function getRecentPosts()
 {
+    $db = getDBConnection();
+    $sth = $db->prepare('SELECT * FROM \'Posts\' LIMIT 5');
+    $sth->execute();
+    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
 
+    if ($result !== false) {
+        return $result;
+    } else {
+        // return that there was an error
+    }
 }
