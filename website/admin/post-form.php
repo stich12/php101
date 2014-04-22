6ddef0c2ea41ad206ccede98f77e837d5536e867
Andre Dublin <81dublin@gmail.com>
2014-04-21 21:34:56 -0400

    update
    Signed-off-by: Andre Dublin <81dublin@gmail.com>

---
+++ website/admin/post-form.php
@@ -0,0 +1,26 @@
+<?php
+require_once 'admin-header.php';
+?>
+
+    <form method="post" action="post-form-submit.php">
+
+        <p>
+            <label for="title">Title</label>
+            <input type="text" name="title" value="<?= isset($_SESSION['title']) ? $_SESSION['title'] : null; ?>"/>
+        </p>
+
+        <p>
+            <label for="content">Content</label>
+            <textarea name="content"
+                      id="content"
+                      cols="30"
+                      rows="10"><?= isset($_SESSION['content']) ? $_SESSION['content'] : null; ?></textarea>
+        </p>
+g 
+        <button type="submit">Save Post</button>
+
+    </form>
+
+<?php
+require_once 'admin-footer.php';
+?>
